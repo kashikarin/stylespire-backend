ddf85ac72abc4ef514465823e0baa0d9fb5098a5
kashikarin <karinkashi@gmail.com>
2025-11-16 15:13:35 +0200

    Initial backend setup

---
+++ server.js
@@ -0,0 +1,18 @@
+import { MongoClient } from 'mongodb'
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import { clarifaiRoutes } from './api/clarifai/clarifai.routes'
+import { recipesRoutes } from './api/recipes/recipes.routes'
+
+
+dotenv.config()
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/clarifai", clarifaiRoutes)
+app.use("/api/recipes", recipesRoutes)
+
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
