1db023d8fec641e04c1c304cd134dc18ffb7d3e1
kashikarin <karinkashi@gmail.com>
2026-01-12 20:03:37 +0200

    fix CORS code for production

--- server.js
+++ server.js
@@ -24,8 +24,6 @@ app.use(cookieParser())
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
-  'http://127.0.0.1:5173',
-  'http://127.0.0.1:5174',
   'https://stylespire-app.onrender.com/'
 ]
 
